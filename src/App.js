b5605e19fbb3b1f361b66fd83cfd98d0f55c5651
chaeeunj <official2930@naver.com>
2022-07-09 21:12:26 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import Navigation from '../components/Navigation';
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <Navigation />
+      프로젝트 시작
+    </div>
+  );
 }
 
 export default App;
