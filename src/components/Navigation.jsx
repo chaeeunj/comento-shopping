f46cca7f391c72c88b621507be7ac115592e6609
chaeeunj <official2930@naver.com>
2022-07-15 04:23:48 +0900

    feat: Navigation 파일에 디자인 적용

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,15 @@
+import styled from 'styled-components';
+
 const Navigation = () => {
   return (
     <div>
-      <div> 코멘토 쇼핑 </div>
+      <TitleStyled> 코멘토 쇼핑 </TitleStyled>
     </div>
   );
 };
 
 export default Navigation;
+
+const TitleStyled = styled.div`
+  padding: 20px 152px;
+`;
