a8f364de27ae7a2015d8276180e0e3f74a1fb602
chaeeunj <official2930@naver.com>
2022-07-15 04:19:14 +0900

    feat: Home 페이지 디자인 적용

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
 const ThemeButton = ({ themeName }) => {
-  return <div>{themeName}</div>;
+  return <ThemeButtonStyled>{themeName}</ThemeButtonStyled>;
 };
 
 export default ThemeButton;
+
+const ThemeButtonStyled = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  width: fit-content;
+  border-radius: 10px;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  padding: 24px 16px;
+  color: white;
+`;
